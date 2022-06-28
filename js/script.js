27c2079c62c2f6271bd131cf61918a9a67839501
Julia Soloveva <yu.soloveva@myhart.ru>
2022-06-28 13:14:39 +0300

    i

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+   const tabWrapper = document.querySelector('.tabheader__items'),
+         tabs = document.querySelectorAll('.tabheader__item'),
+         tabContent = document.querySelectorAll('.tabcontent');
+
+   function hideTabContent() {
+      tabContent.forEach(item => {
+         item.style.display = 'none';
+      });
+
+      tabs.forEach(item => {
+         item.classList.remove('tabheader__item_active');
+      });
+   }
+
+   function showTabContent(i = 0) {
+      tabContent[i].style.display = 'block';
+      tabs[i].classList.add('tabheader__item_active');
+   }
+
+   hideTabContent();
+   showTabContent();
+
+   tabWrapper.addEventListener('click', (event) => {
+      if(event.target && event.target.classList.contains('tabheader__item')) {
+         tabs.forEach((item, i) => {
+            if (event.target == item) {
+               hideTabContent();
+               showTabContent(i);
+            }
+         });
+      }
+   });
+});
